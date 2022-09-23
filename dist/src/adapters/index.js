28cae766f27c51e671a77d49adbf12f1c4d7d4f6
Matheus Azambuja <matheushenrique.ads@gmail.com>
2022-09-22 22:25:14 -0300

    chore: build new version

--- dist/src/adapters/index.js
+++ dist/src/adapters/index.js
@@ -16,4 +16,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./net-create-socket-connection.adapter"), exports);
 __exportStar(require("./string-decoder.adapter"), exports);
+__exportStar(require("./socket-io-server.adapter"), exports);
 //# sourceMappingURL=index.js.map
